0a5ada331f3caa6b5f685179f0af1a07eb2cef62
chennakesavulu.kummari@gmail.com <keshav.kummari@gmail.com>
2019-10-04 08:02:07 +0530

    String-Methods

---
+++ 20191004_DAY-5/string-methods.py
@@ -0,0 +1,53 @@
+# Create a Variable 
+      #  012345678910111213
+#cloud = 'azure aws gcp python aws java aws'
+cloud_1 = 'AWS AZURE GCP'
+cloud_2 = 'AWS azure GCP'
+#print(cloud,type(cloud),id(cloud),len(cloud))
+#print(cloud.capitalize())
+#print(cloud.upper())
+#print(cloud_1.lower())
+#print(cloud.swapcase())
+#print(cloud.isupper())
+#print(cloud_1.isupper())
+#print(cloud_2.isupper())
+#print(cloud.islower())
+#print(cloud_1.islower())
+#print(cloud_2.islower())
+#print(cloud.istitle())
+#print(cloud.isspace())
+#print(cloud.isalpha())
+#print(cloud.isalnum())
+#print(cloud.isnumeric())  # 0 - 9  Only Unicode
+#print(cloud.isdecimal())   # 48-57 decimals  Only Unicode 
+#print(cloud.isdigit())    # 0-9
+
+#print(cloud.count('aws',0,10))
+#print(cloud, len(cloud))
+#print(cloud.find('aws')) # Left to Right Check/Search
+#print(cloud.rfind('aws')) # Right to Left Check/Search
+#print(cloud.startswith('aws'))
+#print(cloud.endswith('aws'))
+cloud = 'azure aws gcp python aws java aws'
+#acloud = 'AZURE AWS GCP PYTHON AWS JAVA AWS'
+#print(cloud.swapcase())
+#print(acloud.title())
+#print(cloud.index('Keshav')) # Left to Right Check/Search
+#print(cloud.rindex('aws')) # Right to Left Check/Search
+#astring = '@#Welcome to Python World!!#'
+#print(astring)
+#print(astring.strip('#'))
+#print(astring.strip('@'))
+#print(astring.lstrip('@#We'))
+#print(astring.rstrip('#'))
+
+# Built-in Functions :
+
+#min()
+#max()
+a = '10 20 30'
+print('Min Value : ',min(a))
+print('Max Value : ',max(a))
+alpha = 'abcABC'
+print('Min Value : ',min(alpha))
+print('Max Value : ',max(alpha))
